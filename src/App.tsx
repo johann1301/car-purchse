a59223e5398d263e3922bc2382214058a133d64c
von Kessel <johannes.von.kessel@accenture.com>
2023-04-11 15:31:03 +0200

    add Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [selectedOption, setSelectedCar] = useState('-');
+  const [currentDate, setCurrentDate] = useState(new Date().toISOString().substring(0, 10));
+
+  const handleFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(e.target.value);
+  };
+
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleCar = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCar(e.target.value);
+  };
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+      <form>
+      <label>
+        First Name:
+        <input type="text" value={firstName} onChange={handleFirstName} />
+      </label>
+      <br />
+      <label>
+        Last Name:
+        <input type="text" value={lastName} onChange={handleLastName} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmail} />
+      </label>
+      <br />
+      <label>
+        Choose a Car:
+        <select value={selectedOption} onChange={handleCar}>
+          <option value="-">-Choose a Car-</option>
+          <option value="Golf">Golf</option>
+          <option value="Atreon">Atreon</option>
+          <option value="Tiguan">Tiguan</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        Current Date:
+        <input type="text" value={currentDate} readOnly />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+
+
       </header>
     </div>
   );
